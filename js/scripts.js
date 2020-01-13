3a57c6f36527df8fb0e57d93bbf0fc4337d2f54b
Rachel Schieferstein and sommer howe <sommer@>
2020-01-13 13:22:23 -0800

    created files

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+
+    
+    var number1 = parseInt(prompt("Enter unit amount."));
+
+    
+
+
+
+var dropstomill = (number1 * 0.051342933268919);
+var milltodrops = (number1 * 19.476877076);
+var galltoliters = (number1 * 3.785411784);
+var literstogall = (number1 * 0.26417205235815);
+var tabletomill = (number1 * 14.78676478125);
+var milltotable = (number1 * 0.067628045403686);
+var teatomill = (number1 * 4.92892159375);
+var milltotea = (number1 * 0.20288413621106);
+
+
+alert(galltoliters);
